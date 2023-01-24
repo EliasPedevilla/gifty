d8b47e904d3bb4d0a938155aa15990f010877d64
Elias Pedevilla <pedevillae@gmail.com>
2023-01-24 00:26:29 -0300

    init with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="w-[full] h-[full] px-6 flex items-center justify-center">
+    <div className="bg-white w-full max-w-[640px] rounded-md my-8 p-3">
+      <p className="font-bold">bienvenida</p>
     </div>
+    
+  </div>
   );
 }
 
